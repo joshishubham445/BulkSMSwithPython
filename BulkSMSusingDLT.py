16c63b13224d803b21b1d3aedec9a38d94c09294
joshishubham445 <79090049+joshishubham445@users.noreply.github.com>
2023-05-12 16:31:47 +0530

    Update BulkSMSusingDLT.py

--- BulkSMSusingDLT.py
+++ BulkSMSusingDLT.py
@@ -1,1 +1,29 @@
+import pandas as pd
+import requests
+from datetime import date
 
+url = "https://www.fast2sms.com/dev/bulkV2"
+
+workbook = pd.read_excel('Add your File Path')
+
+filteredWB = workbook.filter(items=["Name","Mobile Number","Reminder Date"])
+filteredWB["Reminder Date"] = pd.to_datetime(filteredWB["Reminder Date"], format='%Y-%m-%d')
+filteredWB = filteredWB.loc[(filteredWB["Reminder Date"].dt.date == date.today())]
+
+
+for data in filteredWB.index:
+    mobileNo = int(filteredWB["Mobile Number"][data])
+    querystring = {"authorization":"YOUR_API_KEY",
+                   "sender_id":"DLT_SENDER_ID",
+                   "message":"YOUR_MESSAGE_ID",
+                   "variables_values":" Optional",
+                   "route":"dlt",
+                   "numbers":mobileNo}
+
+headers = {
+    'cache-control': "no-cache"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
